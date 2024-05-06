fe6e104d155343c256278bc1525dad1402a368ae
Tatiana Vedeneeva <tatianaved@yandex.ru>
2024-05-06 10:33:25 +0300

    запрашиваем ввод элементов

---
+++ programm.cs
@@ -0,0 +1,9 @@
+sing System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрашиваем у пользователя ввод элементов массива через запятую
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine();
